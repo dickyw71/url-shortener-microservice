e79fb0adfb9283b35114f82f67fc8c846be4e950
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-01-23 08:10:42 +0000

    Added dotenv for reading local env values. Changed GET /new/url to check if url exists in DB and return if found.

--- server.js
+++ server.js
@@ -11,6 +11,7 @@
 // where your node app starts
 
 // init project
+require('dotenv').config();
 var express = require('express');
 var mongodb = require('mongodb');
 var validUrl = require('valid-url');
@@ -18,18 +19,18 @@ var app = express();
 
 var urlShortenerSvc = require('./urlShortener.js');
 
+
 // Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname, details set in .env
-var uri = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.PORT+'/'+process.env.DB;
+var uri = 'mongodb://'+process.env.DBUSER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.PORT+'/'+process.env.DB;
+
+console.log(uri);
 
 mongodb.MongoClient.connect(uri, function(err, db) {
   if(err) throw err;
 });
 
-app.get("/", function (request, response) {
- 
-  var origUrl = "http://freecodecamp.com";
-  response.send(urlShortenerSvc(origUrl));
-  
+app.get("/:urlhash", function (request, response) {
+
 });
 
 app.get("/new/:protocol://:address", (request, response) => {
@@ -37,29 +38,56 @@ app.get("/new/:protocol://:address", (request, response) => {
     var origUrl = request.params.protocol + "://" + request.params.address;
     
     if(validUrl.isWebUri(origUrl)) {
-      console.log(request.params);
-      var shortenedUrlJSON = urlShortenerSvc(origUrl);
 
-      // put the shortenedUrlJSON into the DB
+      var shortenedUrlJSON = { original_url: 'not set', short_url: 'not set'};
+
       mongodb.MongoClient.connect(uri, function(err, db) {
         if(err) throw err;
 
-        // shortenedUrls collection
+         // check if stortened URL is already stored for this URL
         var shortenedUrls = db.collection('shortenedUrls');
+           shortenedUrls.find({
+            original_url: origUrl
+          }, {
+            original_url: 1
+            , short_url: 1
+            , _id: 0       
+        }).toArray(function(err, docs) {
+                if(err) throw err;
 
-        shortenedUrls.find({ original_url: origUrl})
-        shortenedUrls.insert(shortenedUrlJSON, function(err, result) {
+                console.log(docs);
+                shortenedUrlJSON = docs[0]; //  What if there are more docs in array?
 
-          if(err) throw err;
-
-        });
-
-        db.close(function(err) {
-          if(err) throw err;
-        })
+                response.send(shortenedUrlJSON);
+                
+                db.close(function(err) {
+                    if(err) throw err;
+            });
+          })
       });
+
       
-      response.send(shortenedUrlJSON);
+
+      // // put the shortenedUrlJSON into the DB
+      // mongodb.MongoClient.connect(uri, function(err, db) {
+      //   if(err) throw err;
+
+      //   // shortenedUrls collection
+      //   var shortenedUrls = db.collection('shortenedUrls');
+
+      //   shortenedUrls.find({ original_url: origUrl})
+      //   shortenedUrls.insert(shortenedUrlJSON, function(err, result) {
+
+      //     if(err) throw err;
+
+      //   });
+
+      //   db.close(function(err) {
+      //     if(err) throw err;
+      //   })
+      // });
+      
+      
     }
     else {
       response.send(
